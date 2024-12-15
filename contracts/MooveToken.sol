c6e1c2832e5351b4df967c693a43dd7556e6cfe4
b-elia <bordonielia96@gmail.com>
2024-12-15 23:33:24 +0100

    creazione constructor, onlyowner e onlyolderuser e creazione funzione requireOlderUserMint

--- contracts/MooveToken.sol
+++ contracts/MooveToken.sol
@@ -6,31 +6,56 @@ import {ERC20Capped} from "@openzeppelin/contracts/token/ERC20";
 
 contract MooveToken is ERC20Capped {
 
+    uint256 deployTimeStamp;
+    uint256 timestampWeek = 604800;
+
     address public teamAddress;
+    mapping(address => bool) public olderUsersAddresses;
 
     modifier onlyOwner() {
         require(msg.sender == teamAddress, "Not authorized");
         _;
         }
 
+    modifier onlyOlderUsers(){
+        require(olderUsersAddresses[msg.sender] == true, "Not an older user");
+        _;
+    }
+
     //minting sessions
     uint256 public teamMint;    
-    uint256 public olderUsersMint;    
-    uint256 public earlyAdopterMint;    
+    uint256 public olderUsersMintSupply;    
+    uint256 public earlyAdopterMintSupply;    
 
     constructor(
-        string memory name,        // Nome del token
-        string memory symbol,      // Simbolo del token
-        uint256 initialSupply,     // Supply iniziale
-        uint256 cap,                // Massima supply (cap)
-        uint256 olderUsersMintSupply,
-        uint256 earlyAdopterMintSupply
-    ) ERC20(name, symbol) ERC20Capped(cap) {
-        require(initialSupply <= cap, "Initial supply exceeds cap");
+        string memory _name,                // Nome del token
+        string memory _symbol,              // Simbolo del token
+        uint256 _initialSupply,             // Supply iniziale
+        uint256 _cap,                       // Massima supply (cap)
+        uint256 _olderUsersMintSupply,
+        uint256 _earlyAdopterMintSupply,
+        address[] memory _olderUsersAddress
+    ) ERC20(_name, _symbol) ERC20Capped(_cap) {
+        require(_initialSupply <= _cap, "Initial supply exceeds cap");
         teamAddress = msg.sender;
-        _mint(msg.sender, initialSupply);
-        teamMint = initialSupply;
-        olderUsersMint = olderUsersMintSupply;
-        earlyAdopterMint = earlyAdopterMintSupply;
+        _mint(msg.sender, _initialSupply);
+        teamMint = _initialSupply;
+        olderUsersMintSupply = _olderUsersMintSupply;
+        earlyAdopterMintSupply = _earlyAdopterMintSupply;
+
+        for (uint256 i = 0; i < __olderUsersAddresses.length; i++) {
+            olderUsersAddresses[_olderUsersAddresses[i]] = true;
+        }
+
+        deployTimeStamp = block.timestamp / 86400 * 86400;
+    }
+
+    function requireOlderUsersMint() public onlyOlderUsers {
+        uint256 tokenForUser = olderUsersMintSupply / olderUsersAddresses.length;
+
+        require(block.timestamp < deployTimeStamp + (timestampWeek * 4), "The claim period has expired");
+        require(balanceOf(address(this)) >= tokenForUser, "Not enough tokens in contract");
+
+        _transfer(address(this),msg.sender,tokenForUser);
     }
 }
