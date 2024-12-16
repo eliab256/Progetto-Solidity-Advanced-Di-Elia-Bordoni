7fecd1ed8410964061b4f2092a274102f9d92492
b-elia <bordonielia96@gmail.com>
2024-12-16 18:35:58 +0100

    create empty vesting functions and vesting active modifier

--- contracts/MooveToken.sol
+++ contracts/MooveToken.sol
@@ -9,6 +9,7 @@ contract MooveToken is ERC20Capped {
 //variables declaration
     uint256 deployTimeStamp;
     uint256 timestampWeek = 604800;
+    uint8 weeksOfVesting;
     
     //minting sessions
     uint256 public teamMintSupply;    
@@ -16,6 +17,8 @@ contract MooveToken is ERC20Capped {
     uint256 public earlyAdopterMintSupply;  
     
     address public teamAddress;
+
+    bool private vestingPeriod = false;
 
 //Modifiers
     modifier onlyOwner() {
@@ -28,6 +31,11 @@ contract MooveToken is ERC20Capped {
         _;
     }
 
+    modifier activeVestingPeriod(){
+        require(vestingPeriod == true,"there is not a vesting period active");
+        _;
+    }
+
     constructor(
         string memory _name,                    // Token name
         string memory _symbol,                  // Token simbol
@@ -35,7 +43,8 @@ contract MooveToken is ERC20Capped {
         uint256 _cap,                           // Max supply 
         uint256 _olderUsersMintSupply,          // Supply for the older users
         uint256 _earlyAdopterMintSupply,        // Supply the users who interact with the protocol
-        address[] memory _olderUsersAddresses   // Array of older users
+        address[] memory _olderUsersAddresses,  // Array of older users
+        uint8 _weeksOfVesting,                  // Duration of vesting period in weeks     
     ) ERC20(_name, _symbol) ERC20Capped(_cap) {
         require(_teamMintSupply + _olderUsersMintSupply + _earlyAdopterMintSupply  <= _cap, "Initial supply exceeds cap");
         
@@ -45,7 +54,7 @@ contract MooveToken is ERC20Capped {
 
         uint256 remainingTokens;
 
-        if(_olderUsersAddresses > 0){
+        if(_olderUsersAddresses > 0 && _olderUsersMintSupply > 0){
             _mint(address(this), _olderUsersMintSupply);
             olderUsersMintSupply = _olderUsersMintSupply;
             
@@ -59,14 +68,35 @@ contract MooveToken is ERC20Capped {
                 remainingTokens == balanceOf(address(this));
             }
         }
-        earlyAdopterMintSupply = _earlyAdopterMintSupply;
+        
         _mint(address(this), _cap - circulatingSupply() - remainingTokens);
 
         deployTimeStamp = block.timestamp / 86400 * 86400;
 
+        if(_weeksOfVesting > 0 && _earlyAdopterMintSupply > 0){
+            weeksOfVesting = _weeksOfVesting;
+            earlyAdopterMintSupply = _earlyAdopterMintSupply;
+        }
+        
         if(balanceOf(address(this)) + circulatingSupply() != _cap){
             revert("Max supply exceeded");
         }
+    }
+
+    function buyToken() public payable {
+
+    }
+
+    function checkEligibilityClaim() public view {
+
+    }
+
+    function claimCountdown() public view{
+
+    }
+
+    function vestingTokenClaims() public {
+
     }
 
      //require(block.timestamp < deployTimeStamp + (timestampWeek * 4), "The claim period has expired");
