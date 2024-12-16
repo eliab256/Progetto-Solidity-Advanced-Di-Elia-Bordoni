fbd92e81f9b407f8257b050d72d8805851826169
b-elia <bordonielia96@gmail.com>
2024-12-16 23:39:34 +0100

    created eventes for minting and trading status + starting creating buyToken function

--- contracts/MooveToken.sol
+++ contracts/MooveToken.sol
@@ -8,17 +8,22 @@ contract MooveToken is ERC20Capped {
 
 //variables declaration
     uint256 deployTimeStamp;
-    uint256 timestampWeek = 604800;
-    uint8 weeksOfVesting;
+    uint256 private timestampWeek = 604800;
+    uint8 private weeksOfVesting;
     
     //minting sessions
     uint256 public teamMintSupply;    
     uint256 public olderUsersMintSupply;    
     uint256 public earlyAdopterMintSupply;  
     
-    address public teamAddress;
+    address public constant teamAddress;
 
     bool private vestingPeriod = false;
+    bool public isTradingAllowed;
+
+//events
+    event TradingStatusChanged (bool tradingIsAllowed);
+    event TokenMinting (uint256 tokenMintedAmount, uint256 mintingPeriod);
 
 //Modifiers
     modifier onlyOwner() {
@@ -40,11 +45,11 @@ contract MooveToken is ERC20Capped {
         string memory _name,                    // Token name
         string memory _symbol,                  // Token simbol
         uint256 _teamMintSupply,                // Supply for the team
-        uint256 _cap,                           // Max supply 
+        uint256 constant _cap,                  // Max supply 
         uint256 _olderUsersMintSupply,          // Supply for the older users
         uint256 _earlyAdopterMintSupply,        // Supply the users who interact with the protocol
         address[] memory _olderUsersAddresses,  // Array of older users
-        uint8 _weeksOfVesting,                  // Duration of vesting period in weeks     
+        uint8 _weeksOfVesting                   // Duration of vesting period in weeks     
     ) ERC20(_name, _symbol) ERC20Capped(_cap) {
         require(_teamMintSupply + _olderUsersMintSupply + _earlyAdopterMintSupply  <= _cap, "Initial supply exceeds cap");
         
@@ -52,7 +57,7 @@ contract MooveToken is ERC20Capped {
         _mint(msg.sender, _teamMintSupply);
         teamMintSupply = _teamMintSupply;
 
-        uint256 remainingTokens;
+        uint256 internal remainingTokens;
 
         if(_olderUsersAddresses > 0 && _olderUsersMintSupply > 0){
             _mint(address(this), _olderUsersMintSupply);
@@ -81,11 +86,31 @@ contract MooveToken is ERC20Capped {
         if(balanceOf(address(this)) + circulatingSupply() != _cap){
             revert("Max supply exceeded");
         }
+
+        uint256 private totalMintedToken = balanceOf(address(this)) + circulatingSupply();
+        emit TokenMinting(totalMintedToken, deployTimeStamp);
+    }
+
+    function enableTrading() external onlyOwner {
+        isTradingAllowed = !isTradingAllowed; 
+        emit TradingStatusChanged(isTradingAllowed);
     }
 
     function buyToken() public payable {
+        require(msg.value > 0, "Insufficient funds");
+        require(msg.value < balanceOf(address(this)), "there is no enough token on the contract");
+        require(isTradingAllowed == true, "team doesn't allolow trading");
+
+        //trasferire token al buyer, aggiornare i saldi e fare la matematica per la conversione tra token
+
+        updateElegibleAdresses(msg.sender);
 
     }
+
+    function updateElegibleAdresses(address _buyerAddress) private activeVestingPeriod {
+        
+    }
+
 
     function checkEligibilityClaim() public view {
 
