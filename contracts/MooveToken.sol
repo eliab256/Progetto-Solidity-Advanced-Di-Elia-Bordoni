d6b9ed88ee7640440e7f5cab5c55347e96c28e76
b-elia <bordonielia96@gmail.com>
2024-12-16 17:52:50 +0100

    constructor conclusion

--- contracts/MooveToken.sol
+++ contracts/MooveToken.sol
@@ -10,9 +10,12 @@ contract MooveToken is ERC20Capped {
     uint256 deployTimeStamp;
     uint256 timestampWeek = 604800;
     
-
+    //minting sessions
+    uint256 public teamMintSupply;    
+    uint256 public olderUsersMintSupply;    
+    uint256 public earlyAdopterMintSupply;  
+    
     address public teamAddress;
-    mapping(address => bool) public olderUsersAddresses;
 
 //Modifiers
     modifier onlyOwner() {
@@ -20,19 +23,10 @@ contract MooveToken is ERC20Capped {
         _;
         }
 
-    modifier onlyOlderUsers(){
-        require(olderUsersAddresses[msg.sender] == true, "Not an older user");
-        _;
-    }
     modifier maxSupplyNotReached(){
         require(circulatingSupply() + balanceOf(address(this)) < _cap, "Max supply reached");
         _;
     }
-
-//minting sessions
-    uint256 public teamMintSupply;    
-    uint256 public olderUsersMintSupply;    
-    uint256 public earlyAdopterMintSupply;    
 
     constructor(
         string memory _name,                    // Token name
@@ -41,29 +35,39 @@ contract MooveToken is ERC20Capped {
         uint256 _cap,                           // Max supply 
         uint256 _olderUsersMintSupply,          // Supply for the older users
         uint256 _earlyAdopterMintSupply,        // Supply the users who interact with the protocol
-        address[] memory _olderUsersAddress     // Array of older users
+        address[] memory _olderUsersAddresses   // Array of older users
     ) ERC20(_name, _symbol) ERC20Capped(_cap) {
         require(_teamMintSupply + _olderUsersMintSupply + _earlyAdopterMintSupply  <= _cap, "Initial supply exceeds cap");
+        
         teamAddress = msg.sender;
         _mint(msg.sender, _teamMintSupply);
         teamMintSupply = _teamMintSupply;
-        _mint(address(this), _olderUsersMintSupply);
-        olderUsersMintSupply = _olderUsersMintSupply;
-        earlyAdopterMintSupply = _earlyAdopterMintSupply;
 
-        for (uint256 i = 0; i < _olderUsersAddresses.length; i++) {
-            olderUsersAddresses[_olderUsersAddresses[i]] = true;
+        uint256 remainingTokens;
+
+        if(_olderUsersAddresses > 0){
+            _mint(address(this), _olderUsersMintSupply);
+            olderUsersMintSupply = _olderUsersMintSupply;
+            
+            uint256 tokenForUser = olderUsersMintSupply / olderUsersAddresses.length;
+            for (uint256 i = 0; i < _olderUsersAddresses.length; i++) {   
+                require(balanceOf(address(this)) >= tokenForUser, "Not enough tokens in contract");
+                _transfer(address(this),_olderUsersAddresses[i],tokenForUser);
+            }
+
+            if(balanceOf(address(this)) > 0){
+                remainingTokens == balanceOf(address(this));
+            }
         }
+        earlyAdopterMintSupply = _earlyAdopterMintSupply;
+        _mint(address(this), _cap - circulatingSupply() - remainingTokens);
 
         deployTimeStamp = block.timestamp / 86400 * 86400;
+
+        if(balanceOf(address(this)) + circulatingSupply() != _cap){
+            revert("Max supply exceeded");
+        }
     }
 
-    function requireOlderUsersMint() public onlyOlderUsers {
-        uint256 tokenForUser = olderUsersMintSupply / olderUsersAddresses.length;
-
-        require(block.timestamp < deployTimeStamp + (timestampWeek * 4), "The claim period has expired");
-        require(balanceOf(address(this)) >= tokenForUser, "Not enough tokens in contract");
-
-        _transfer(address(this),msg.sender,tokenForUser);
-    }
+     //require(block.timestamp < deployTimeStamp + (timestampWeek * 4), "The claim period has expired");
 }
