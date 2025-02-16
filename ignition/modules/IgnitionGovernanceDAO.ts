ccf2e9a44336094f2c333ba731609badf0d19ffc
b-elia <bordonielia96@gmail.com>
2025-02-16 14:39:10 +0100

    ignition deploy need to be fixed

--- ignition/modules/IgnitionGovernanceDAO.ts
+++ ignition/modules/IgnitionGovernanceDAO.ts
@@ -1,14 +1,15 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import args from "../../scripts/arguments";
 
-const constructorArgs = args;
-
 const ONE_ETH: bigint = 1_000_000_000_000_000_000n;
 
 const GovernanceDAOModule = buildModule("GovernanceDAOModule", (m) => {
   const initialBalance = m.getParameter("InitialBalance", ONE_ETH);
+
+  const constructorArgs = args;
+
   //controllare come passare args
-  const governanceDAO = m.contract("GovernanceDAO", args, {
+  const governanceDAO = m.contract("GovernanceDAO", constructorArgs, {
     value: initialBalance,
   });
 
--- scripts/deployIgnition.ts
+++ scripts/deployIgnition.ts
@@ -2,7 +2,10 @@ import hre from "hardhat";
 import GovernanceDAOModule from "../ignition/modules/IgnitionGovernanceDAO";
 
 async function main() {
-  const { governanceDAO } = await hre.ignition.deploy(GovernanceDAOModule, {});
+  console.log("Deploying GovernanceDAO...");
+  const { governanceDAO } = await hre.ignition.deploy(GovernanceDAOModule);
+
+  console.log("GovernanceDAO deployed at:", await governanceDAO.target);
 }
 
 main().catch((error) => {
